9c661db99a68f634474f730358f0ce7fddd64d62
Frederico Curto <fcurto@brex.com>
2023-11-11 05:44:03 -0300

    update manifest app name

--- src/manifest.js
+++ src/manifest.js
@@ -2,7 +2,7 @@ import { defineManifest } from '@crxjs/vite-plugin'
 import packageData from '../package.json'
 
 export default defineManifest({
-  name: packageData.name,
+  name: 'Github PR Tables',
   description: packageData.description,
   version: packageData.version,
   manifest_version: 3,
